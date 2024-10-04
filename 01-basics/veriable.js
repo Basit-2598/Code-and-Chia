338f522e29e846004bf12d7800d0f362b8f8a472
Basit Ali <basitali2598@gmail.com>
2024-10-04 10:17:22 +0000

    container config

---
+++ 01-basics/veriable.js
@@ -0,0 +1,8 @@
+const accountId = 67676
+let acoonutName ="basit"
+var accountCity = "Mardan"
+let basit;
+
+console.log("accountId" + accountId)
+
+console.table([accountCity, accountId, acoonutName, basit])
